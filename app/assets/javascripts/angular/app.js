c856410d0732270e902752d038b49a9420bb8c77
zachnewburgh <zach.newburgh@gmail.com>
2016-09-07 16:39:43 -0400

    Injecting templates into the app

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -1,5 +1,5 @@
 angular
-  .module('app', ['ui.router', 'ngResource'])
+  .module('app', ['ui.router', 'ngResource', 'templates'])
   .config(function($stateProvider, $urlRouterProvider) {
     
     $stateProvider
