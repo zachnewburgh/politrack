bfb7383771c462f13df4a08a212325d4206534ed
zachnewburgh <zach.newburgh@gmail.com>
2016-09-08 11:05:30 -0400

    Returning Politician

--- app/assets/javascripts/angular/models/politician.js
+++ app/assets/javascripts/angular/models/politician.js
@@ -2,6 +2,7 @@ function Politician($resource) {
   var Politician = $resource('http://localhost:3000/api/v1/politicians/:id.json', {id: '@id'}, {
     update: {method: 'PUT'}
   });
+  return Politician;
 };
 
 angular
