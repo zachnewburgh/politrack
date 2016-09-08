023a01c7e65a0cc85d3e246b6ef7fd69daacd4b6
zachnewburgh <zach.newburgh@gmail.com>
2016-09-08 11:01:04 -0400

    Changed hard-coded ctrl.politicians array to query Politician

--- app/assets/javascripts/angular/controllers/PoliticiansController.js
+++ app/assets/javascripts/angular/controllers/PoliticiansController.js
@@ -1,16 +1,6 @@
-function PoliticiansController() {
+function PoliticiansController(Politician) {
   var ctrl = this;
-
-  ctrl.politicians = [
-    {
-      name: 'Barack Obama',
-      bio: '44th U.S. President'
-    },
-    {
-      name: 'Hillary Clinton',
-      bio: '67th U.S. Secretary of State'
-    }
-  ];
+  ctrl.politicians = Politician.query();
 }
 
 angular
