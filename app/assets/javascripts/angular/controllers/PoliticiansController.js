39b320010536302d00090ffadf4f21b0bc7111d7
zachnewburgh <zach.newburgh@gmail.com>
2016-09-09 12:06:19 -0400

    Added deletePolitician function to delete Politician

--- app/assets/javascripts/angular/controllers/PoliticiansController.js
+++ app/assets/javascripts/angular/controllers/PoliticiansController.js
@@ -1,7 +1,13 @@
 function PoliticiansController(Politician) {
   var ctrl = this;
   ctrl.politicians = Politician.query();
-}
+
+  ctrl.deletePolitician = function(politician) {
+    politician.$delete(function() {
+      $state.go($state.current, {}, {reload: true});
+    });
+  };
+};
 
 angular
   .module('app')
