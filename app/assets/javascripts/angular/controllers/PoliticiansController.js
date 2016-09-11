4500feb99d26d0fefe7355430cdf9330664d7862
zachnewburgh <zach.newburgh@gmail.com>
2016-09-11 17:53:57 -0400

    Added upvotePolitician function that adds 1 to the Politician's popularity

--- app/assets/javascripts/angular/controllers/PoliticiansController.js
+++ app/assets/javascripts/angular/controllers/PoliticiansController.js
@@ -2,6 +2,10 @@ function PoliticiansController(Politician, $location, $state) {
   var ctrl = this;
   ctrl.politicians = Politician.query();
 
+  ctrl.upvotePolitician = function(politician) {
+    ctrl.politician.popularity = ctrl.politician.popularity + 1;
+  }
+
   ctrl.deletePolitician = function(politician) {
     politician.$delete(function() {
       $state.reload();
