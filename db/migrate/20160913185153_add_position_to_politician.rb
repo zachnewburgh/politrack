4629a3b33216219204184fd9eef55caa290c5aa0
zachnewburgh <zach.newburgh@gmail.com>
2016-09-13 14:53:07 -0400

    Replaced migration to add position to Politician, so that the attribute would be a string instead of an integer

--- db/migrate/20160913185153_add_position_to_politician.rb
+++ db/migrate/20160913185153_add_position_to_politician.rb
@@ -1,5 +1,5 @@
 class AddPositionToPolitician < ActiveRecord::Migration[5.0]
   def change
-    add_column :politicians, :position, :integer
+    add_column :politicians, :position, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160911215533) do
+ActiveRecord::Schema.define(version: 20160913183625) do
 
   create_table "politicians", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20160911215533) do
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
     t.integer  "popularity", default: 0
+    t.integer  "position"
   end
 
 end
