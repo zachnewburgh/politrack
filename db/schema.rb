b60c87a5c6b3453e2128c9c203cc3212c0b92084
zachnewburgh <zach.newburgh@gmail.com>
2016-09-13 14:56:02 -0400

    Changed position attribute to string from integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160913183625) do
+ActiveRecord::Schema.define(version: 20160913185153) do
 
   create_table "politicians", force: :cascade do |t|
     t.string   "name"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20160913183625) do
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
     t.integer  "popularity", default: 0
-    t.integer  "position"
+    t.string   "position"
   end
 
 end
