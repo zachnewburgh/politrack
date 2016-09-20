f476eef8ffe2e0c4177547d09676d8d00a886520
zachnewburgh <zach.newburgh@gmail.com>
2016-09-20 10:33:16 -0400

    Changed spaceing

--- app/assets/javascripts/angular/services/PoliticianService.js
+++ app/assets/javascripts/angular/services/PoliticianService.js
@@ -1,19 +1,23 @@
 function PoliticianService($http) {
   
-  this.getPoliticians = function () {
+  this.getPoliticians = function() {
     return $http.get('http://localhost:3000/api/v1/politicians');
   };
 
-  this.getPolitician = function (politician) {
+  this.getPolitician = function(politician) {
     return $http.get('http://localhost:3000/api/v1/politicians/' + politician.id);
   };
 
-  this.createPolitician= function (politician) {
+  this.createPolitician= function(politician) {
     return $http.post('http://localhost:3000/api/v1/politicians/' + politician.id);
   };
 
-  this.updatePolitician= function (politician) {
+  this.updatePolitician= function(politician) {
     return $http.post('http://localhost:3000/api/v1/politicians/' + politician.id);
+  };
+
+  this.deletePolitician= function(politician) {
+    return $http.delete('http://localhost:3000/api/v1/politicians/' + politician.id);
   };
 }
 
