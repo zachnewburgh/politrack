1f11adc6186e3fbb45dca72b11efee14a169a5c0
zachnewburgh <zach.newburgh@gmail.com>
2016-09-20 10:32:19 -0400

    Created PoliticianService with routes for CRUD

---
+++ app/assets/javascripts/angular/services/PoliticianService.js
@@ -0,0 +1,22 @@
+function PoliticianService($http) {
+  
+  this.getPoliticians = function () {
+    return $http.get('http://localhost:3000/api/v1/politicians');
+  };
+
+  this.getPolitician = function (politician) {
+    return $http.get('http://localhost:3000/api/v1/politicians/' + politician.id);
+  };
+
+  this.createPolitician= function (politician) {
+    return $http.post('http://localhost:3000/api/v1/politicians/' + politician.id);
+  };
+
+  this.updatePolitician= function (politician) {
+    return $http.post('http://localhost:3000/api/v1/politicians/' + politician.id);
+  };
+}
+
+angular
+  .module('app')
+  .service('UserService', UserService);
