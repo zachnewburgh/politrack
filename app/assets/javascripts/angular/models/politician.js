de72676cd9c68e917b9dd674abafeb7fed8e6a5a
zachnewburgh <zach.newburgh@gmail.com>
2016-09-20 22:46:10 -0400

    Commented out the Politician model to switch from resource use to http requests

--- app/assets/javascripts/angular/models/politician.js
+++ app/assets/javascripts/angular/models/politician.js
@@ -1,10 +1,10 @@
-function Politician($resource) {
-  var Politician = $resource('http://localhost:3000/api/v1/politicians/:id.json', {id: '@id'}, {
-    update: {method: 'PUT'}
-  });
-  return Politician;
-};
+// function Politician($resource) {
+//   var Politician = $resource('http://localhost:3000/api/v1/politicians/:id.json', {id: '@id'}, {
+//     update: {method: 'PUT'}
+//   });
+//   return Politician;
+// };
 
-angular
-  .module('app')
-  .factory('Politician', Politician);
+// angular
+//   .module('app')
+//   .factory('Politician', Politician);
