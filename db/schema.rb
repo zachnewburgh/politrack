695930468b0860eec90ee1fb06f15cdd9141c505
zachnewburgh <zach.newburgh@gmail.com>
2016-09-21 14:31:05 -0400

    Migrated the schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160919184303) do
+ActiveRecord::Schema.define(version: 20160921182109) do
 
   create_table "politicians", force: :cascade do |t|
     t.string   "name"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 20160919184303) do
     t.string   "position"
     t.string   "government_level"
     t.string   "homestate"
+  end
+
+  create_table "states", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
