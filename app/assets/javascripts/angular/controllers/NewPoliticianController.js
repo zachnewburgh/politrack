df3b148497254f537d65433754b1fc581e5c6dd6
zachnewburgh <zach.newburgh@gmail.com>
2016-09-21 14:48:13 -0400

    Added StateService.getStates

--- app/assets/javascripts/angular/controllers/NewPoliticianController.js
+++ app/assets/javascripts/angular/controllers/NewPoliticianController.js
@@ -1,6 +1,11 @@
 function NewPoliticianController($location, PoliticianService) {
   var ctrl = this;
 
+  StateService.getStates()
+    .then(function(response) {
+      ctrl.states = response.data;
+    });
+
   ctrl.addPolitician = function() {
     var data = {name: this.politician.name, position: this.politician.position, homestate: this.politician.homestate};
     PoliticianService.createPolitician(data);
