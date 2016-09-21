ce98c2ec73fa49301c68d6209c23ec2e71d11ba1
zachnewburgh <zach.newburgh@gmail.com>
2016-09-21 14:57:13 -0400

    Added PoliticianService.getPoliticians

--- app/assets/javascripts/angular/controllers/ViewStateController.js
+++ app/assets/javascripts/angular/controllers/ViewStateController.js
@@ -1,10 +1,15 @@
-function ViewStateController(StateService, $stateParams) {
+function ViewStateController(StateService, $stateParams, PoliticianService) {
   var ctrl = this;
 
   StateService.getState($stateParams.id)
     .then(function(response) {
       ctrl.state = response.data
-    })
+    });
+
+  PoliticianService.getPoliticians()
+    .then(function(response) {
+      ctrl.politicians = response.data
+    });
 }
 
 angular
