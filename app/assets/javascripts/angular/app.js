adafdf162fb3d438a7046c2a885b897292c7e5d5
zachnewburgh <zach.newburgh@gmail.com>
2016-09-22 17:08:48 -0400

    Changed templateUrl in each state to account for new state and politician directories

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -10,32 +10,32 @@ angular
       })
       .state('home.main', {
         url: 'main',
-        templateUrl: 'home/main.html',
+        templateUrl: 'home/politician/main.html',
         controller: 'MainController as ctrl'
       })
       .state('home.new', {
         url: 'new',
-        templateUrl: 'home/new.html',
+        templateUrl: 'home/politician/new.html',
         controller: 'NewPoliticianController as ctrl'
       })
       .state('home.politicians', {
         url: 'politicians',
-        templateUrl: 'home/politicians.html',
+        templateUrl: 'home/politician/politicians.html',
         controller: 'PoliticiansController as ctrl'
       })
       .state('home.politician', {
         url: 'politician/:id',
-        templateUrl: 'home/show.html',
+        templateUrl: 'home/politician/show.html',
         controller: 'ViewPoliticianController as ctrl'
       })
       .state('home.edit', {
         url: 'edit/:id',
-        templateUrl: 'home/edit.html',
+        templateUrl: 'home/politician/edit.html',
         controller: 'EditPoliticianController as ctrl'
       })
       .state('home.state', {
         url: 'state/:id',
-        templateUrl: 'home/state.html',
+        templateUrl: 'home/state/state.html',
         controller: 'ViewStateController as ctrl'
       });
 
