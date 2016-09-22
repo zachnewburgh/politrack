b8540ff082c116933fba742abe524f269810695b
zachnewburgh <zach.newburgh@gmail.com>
2016-09-22 13:21:45 -0400

    Added ctrl.editPolitician function and injected state into the Controller

--- app/assets/javascripts/angular/controllers/ViewPoliticianController.js
+++ app/assets/javascripts/angular/controllers/ViewPoliticianController.js
@@ -1,10 +1,17 @@
-function ViewPoliticianController(PoliticianService, $stateParams) {
+function ViewPoliticianController(PoliticianService, $stateParams, $state) {
   var ctrl = this;
 
   PoliticianService.getPolitician($stateParams.id)
     .then(function(response) {
       ctrl.politician = response.data
     })
+
+  ctrl.editPolitician = function() {
+    var data = {homestate: this.politician.homestate};
+    PoliticianService.updatePolitician(ctrl.politician.id, data);
+    $state.reload();
+  };
+
 }
 
 angular
